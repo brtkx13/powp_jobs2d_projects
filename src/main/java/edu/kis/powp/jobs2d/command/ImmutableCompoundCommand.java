2f4b6f6c2befc82180d033a464e0a8001674021d
brtkx13 <107516304+brtkx13@users.noreply.github.com>
2026-05-20 11:56:05 +0200

    add visitor for a deep copy of complex command

--- src/main/java/edu/kis/powp/jobs2d/command/ImmutableCompoundCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ImmutableCompoundCommand.java
@@ -84,10 +84,8 @@ public class ImmutableCompoundCommand implements ICompoundCommand {
 
     @Override
     public ImmutableCompoundCommand deepCopy() {
-        List<DriverCommand> copiedCommands = new ArrayList<>();
-        for (DriverCommand cmd : commands) {
-            copiedCommands.add(cmd.deepCopy());
-        }
-        return new ImmutableCompoundCommand(this.name, copiedCommands);
+        edu.kis.powp.jobs2d.command.visitor.CommandDeepCopyVisitor visitor = new edu.kis.powp.jobs2d.command.visitor.CommandDeepCopyVisitor();
+        this.accept(visitor);
+        return (ImmutableCompoundCommand) visitor.getCopy();
     }
 }
